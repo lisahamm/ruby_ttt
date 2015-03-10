771acf0c372396976150ee1961240d536b36a192
Lisa Hamm <lisalhamm@gmail.com>
2015-03-10 17:28:12 -0500

    Added board with default value to Game initialization arguments

--- lib/tic_tac_toe/game.rb
+++ lib/tic_tac_toe/game.rb
@@ -3,8 +3,8 @@ module TicTacToe
     attr_accessor :board, :current_player
     attr_reader :player1, :player2
 
-    def initialize(player_settings)
-      @board = Board.new
+    def initialize(board=nil, player_settings)
+      @board = board ||= Board.new
       @player1 = set_player(0, player_settings)
       @player2 = set_player(1, player_settings)
       @current_player = player1
@@ -18,13 +18,12 @@ module TicTacToe
 
     def check_first_move(player_settings)
       if player_settings[0].fetch(:ai) == true
-        take_turn
+        board.set_cell(generate_ai_move, current_player.mark)
         switch_turn
       end
     end
 
-    def take_turn(cell_number=nil)
-      cell_number = cell_number ||= generate_ai_move(board)
+    def take_turn(cell_number)
       board.set_cell(cell_number, current_player.mark)
     end
 
--- spec/game_spec.rb
+++ spec/game_spec.rb
@@ -2,38 +2,45 @@ require "spec_helper"
 
 module TicTacToe
   describe Game do
-    let(:player_settings) {[{mark: 'X', ai: false}, {mark: 'O', ai: true}]
-}
-    let(:game) {Game.new(player_settings)}
+    let(:player_settings1) {[{mark: 'X', ai: false}, {mark: 'O', ai: true}]}
+    let(:player_settings2) {[{mark: 'X', ai: true}, {mark: 'O', ai: false}]}
+    let(:player_settings3) {[{mark: 'X', ai: false}, {mark: 'O', ai: false}]}
+    let(:game) {Game.new(player_settings1)}
 
-    # let(:game_in_play) do
-    #   cells = ['O', 'O', nil,
-    #            nil, 'X', nil,
-    #            nil, nil, nil]
-    #   board = Board.new(cells: cells)
-    #   Game.new(board, 'X', 'O')
-    # end
 
-    # let(:won_game) do
-    #   cells = ['X', 'X', 'O',
-    #            'O', 'X', 'X',
-    #            'X', 'O', 'X']
-    #   board = Board.new(cells: cells)
-    #   Game.new(board, 'X', 'O')
-    # end
+    let(:game_in_play) do
+      cells = ['O', 'O', nil,
+               nil, 'X', nil,
+               nil, nil, nil]
+      board = Board.new(cells: cells)
+      Game.new(board, player_settings3)
+    end
 
-    # let(:tie_game) do
-    #   cells = ['X', 'X', 'O',
-    #            'O', 'O', 'X',
-    #            'X', 'O', 'X']
-    #   board = Board.new(cells: cells)
-    #   Game.new(board, 'X', 'O')
-    # end
+    let(:won_game) do
+      cells = ['X', 'X', 'O',
+               'O', 'X', 'X',
+               'X', 'O', 'X']
+      board = Board.new(cells: cells)
+      Game.new(board, player_settings3)
+    end
+
+    let(:tie_game) do
+      cells = ['X', 'X', 'O',
+               'O', 'O', 'X',
+               'X', 'O', 'X']
+      board = Board.new(cells: cells)
+      Game.new(board, player_settings3)
+    end
 
     describe "#take_turn" do
       it "adds the current player's mark to specified cell" do
         game.take_turn(0)
         expect(game.board.get_cell(0)).to eq game.current_player.mark
+      end
+
+      it "adds an ai move to the board when computer opponent is the first player" do
+        game2 = Game.new(player_settings2)
+        expect(game2.board.empty?).to eq false
       end
     end
 
@@ -58,46 +65,46 @@ module TicTacToe
       end
     end
 
-    # # describe "#winner?" do
-    # #   it "is false when in play" do
-    # #     expect(game_in_play.winner?).to eq false
-    # #   end
+    describe "#winner?" do
+      it "is false when in play" do
+        expect(game_in_play.winner?).to eq false
+      end
 
-    # #   it "is true when won" do
-    # #     expect(won_game.winner?).to eq true
-    # #   end
+      it "is true when won" do
+        expect(won_game.winner?).to eq true
+      end
 
-    # #   it "is false when tied" do
-    # #     expect(tie_game.winner?).to eq false
-    # #   end
-    # # end
+      it "is false when tied" do
+        expect(tie_game.winner?).to eq false
+      end
+    end
 
-    # # describe "#get_winning_player" do
-    # #   it "is winner's mark when won" do
-    # #     expect(won_game.get_winning_player.mark).to eq 'X'
-    # #   end
+    describe "#get_winning_player" do
+      it "is winner's mark when won" do
+        expect(won_game.get_winning_player.mark).to eq 'X'
+      end
 
-    # #   it "is nil when tied" do
-    # #     expect(tie_game.get_winning_player).to eq nil
-    # #   end
+      it "is nil when tied" do
+        expect(tie_game.get_winning_player).to eq nil
+      end
 
-    # #   it "is nil when in play" do
-    # #     expect(game_in_play.get_winning_player).to eq nil
-    # #   end
-    # # end
+      it "is nil when in play" do
+        expect(game_in_play.get_winning_player).to eq nil
+      end
+    end
 
-    # describe "#tie?" do
-    #   it "is false when in play" do
-    #     expect(game_in_play.tie?).to eq false
-    #   end
+    describe "#tie?" do
+      it "is false when in play" do
+        expect(game_in_play.tie?).to eq false
+      end
 
-    #   it "is true when tied" do
-    #     expect(tie_game.tie?).to eq true
-    #   end
+      it "is true when tied" do
+        expect(tie_game.tie?).to eq true
+      end
 
-    #   it "is false when won" do
-    #     expect(won_game.tie?).to eq false
-    #   end
-    # end
+      it "is false when won" do
+        expect(won_game.tie?).to eq false
+      end
+    end
   end
 end
